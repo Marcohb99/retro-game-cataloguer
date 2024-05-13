4684daaba7be6cd22ccfcfc5281f4ad94e32948c
Marco Hurtado <mhurtado@freepik.com>
2024-05-13 10:10:39 +0200

    feat(PriceCalculator): use a sorted map and add test with providers

--- src/main/java/com/mhb/retrogamecataloguerapi/Domain/Price/PriceCalculator.java
+++ src/main/java/com/mhb/retrogamecataloguerapi/Domain/Price/PriceCalculator.java
@@ -2,27 +2,31 @@ package com.mhb.retrogamecataloguerapi.Domain.Price;
 
 import com.mhb.retrogamecataloguerapi.Domain.Game.Game;
 
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
 
 public class PriceCalculator {
-    Map<Integer, Float> priceMap;
+    private final Map<Integer, Float> priceMap;
+    private final Float minimumPrice;
 
-    public PriceCalculator(Map<Integer, Float> priceMap) {
+    public PriceCalculator(SortedMap<Integer, Float> priceMap, Float minimumPrice) {
         this.priceMap = priceMap;
+        this.minimumPrice = minimumPrice;
     }
 
     public float calculatePrice(Game game, Integer units) {
-        Set<Integer> unitPrices = priceMap.keySet();
-        float unitPrice = 0.0f;
-        while (unitPrices.size() > 0) {
-            Integer next = unitPrices.iterator().next();
-            if (units < next) {
-                unitPrice = priceMap.get(next);
-                break;
-            }
-            unitPrices.remove(next);
+        if (units == 0) {
+            return 0.0f;
         }
-        return unitPrice * units;
+
+        for (Map.Entry<Integer, Float> entry : this.priceMap.entrySet()) {
+            if (units <= entry.getKey()) {
+                return entry.getValue() * units;
+            }
+        }
+
+        return this.minimumPrice * units;
     }
 }
--- src/test/java/com/mhb/retrogamecataloguerapi/Domain/Price/PriceCalculatorTest.java
+++ src/test/java/com/mhb/retrogamecataloguerapi/Domain/Price/PriceCalculatorTest.java
@@ -2,28 +2,49 @@ package com.mhb.retrogamecataloguerapi.Domain.Price;
 
 import com.mhb.retrogamecataloguerapi.Domain.Game.Game;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class PriceCalculatorTest {
-    @Test
-    public void testPriceCalculator() {
-        PriceCalculator priceCalculator = new PriceCalculator(
-                Map.of(
-                        2, 299.0f,
-                        10, 239.0f,
-                        25, 219.0f,
-                        50, 199.0f,
-                        100, 179.0f
-                        )
-        );
+    static Collection<Object[]> provideUnitsAndExpectedPrice() {
+        return Arrays.asList(new Object[][] {
+                {0, 0.0f},
+                {1, 299.0f},
+                {2, 598.0f},
+                {5, 1195.0f},
+                {10, 2390.0f},
+                {15, 3285.0f},
+                {20, 4380.0f},
+                {25, 5475.0f},
+                {30, 5970.0f},
+                {50, 9950.0f},
+                {100, 17900.0f},
+        });
+    }
+    @ParameterizedTest
+    @MethodSource("provideUnitsAndExpectedPrice")
+    public void testPriceCalculator(Integer units, Float expectedPrice) {
+        // Given
+        SortedMap<Integer, Float> priceMap = new TreeMap<>();
+        priceMap.put(2, 299.0f);
+        priceMap.put(10, 239.0f);
+        priceMap.put(25, 219.0f);
+        priceMap.put(50, 199.0f);
+        priceMap.put(100, 179.0f);
+        PriceCalculator priceCalculator = new PriceCalculator(priceMap, 179.0f);
         Price price = new Price(123);
         Game game = new Game("Super Mario Bros", price);
-        float result = priceCalculator.calculatePrice(game, 20);
-        assertEquals(4380, result);
+
+        // When
+        float result = priceCalculator.calculatePrice(game, units);
+
+        // Then
+        assertEquals(expectedPrice, result);
     }
 }
