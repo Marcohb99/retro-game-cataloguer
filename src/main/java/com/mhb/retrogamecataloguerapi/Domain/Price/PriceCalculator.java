50e8bee8d591bf7bcb5ad9fc398d09d4a107b847
Marco Hurtado <mhurtado@freepik.com>
2024-05-13 09:30:00 +0200

    feat(PriceCalculator): implement first version of PriceCalculator

--- src/main/java/com/mhb/retrogamecataloguerapi/Domain/Price/PriceCalculator.java
+++ src/main/java/com/mhb/retrogamecataloguerapi/Domain/Price/PriceCalculator.java
@@ -2,9 +2,27 @@ package com.mhb.retrogamecataloguerapi.Domain.Price;
 
 import com.mhb.retrogamecataloguerapi.Domain.Game.Game;
 
+import java.util.Map;
+import java.util.Set;
+
 public class PriceCalculator {
+    Map<Integer, Float> priceMap;
+
+    public PriceCalculator(Map<Integer, Float> priceMap) {
+        this.priceMap = priceMap;
+    }
 
     public float calculatePrice(Game game, Integer units) {
-        return 2460;
+        Set<Integer> unitPrices = priceMap.keySet();
+        float unitPrice = 0.0f;
+        while (unitPrices.size() > 0) {
+            Integer next = unitPrices.iterator().next();
+            if (units < next) {
+                unitPrice = priceMap.get(next);
+                break;
+            }
+            unitPrices.remove(next);
+        }
+        return unitPrice * units;
     }
 }
--- src/test/java/com/mhb/retrogamecataloguerapi/Domain/Price/PriceCalculatorTest.java
+++ src/test/java/com/mhb/retrogamecataloguerapi/Domain/Price/PriceCalculatorTest.java
@@ -4,16 +4,26 @@ import com.mhb.retrogamecataloguerapi.Domain.Game.Game;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class PriceCalculatorTest {
     @Test
     public void testPriceCalculator() {
-        PriceCalculator priceCalculator = new PriceCalculator();
+        PriceCalculator priceCalculator = new PriceCalculator(
+                Map.of(
+                        2, 299.0f,
+                        10, 239.0f,
+                        25, 219.0f,
+                        50, 199.0f,
+                        100, 179.0f
+                        )
+        );
         Price price = new Price(123);
         Game game = new Game("Super Mario Bros", price);
         float result = priceCalculator.calculatePrice(game, 20);
-        assertEquals(2460, result);
+        assertEquals(4380, result);
     }
 }
